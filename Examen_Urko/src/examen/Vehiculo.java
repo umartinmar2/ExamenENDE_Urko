ebe9da183c4e55d4970979ff65c53485ab4d5f64
umartinmar2 <umartinmar2@educacion.navarra.es>
2025-12-19 09:05:07 +0100

    Metodos añadidos

--- Examen_Urko/src/examen/Vehiculo.java
+++ Examen_Urko/src/examen/Vehiculo.java
@@ -14,6 +14,53 @@ public class Vehiculo {
 		this.modelo = modelo;
 		this.precioHora = precioHora;
 	}
+
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+
+	public String getMarca() {
+		return marca;
+	}
+
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+
+	public String getModelo() {
+		return modelo;
+	}
+
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+
+	public double getPrecioHora() {
+		return precioHora;
+	}
+
+
+	public void setPrecioHora(double precioHora) {
+		this.precioHora = precioHora;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Vehiculo [matricula=" + matricula + ", marca=" + marca + ", modelo=" + modelo + ", precioHora="
+				+ precioHora + "]";
+	}
 	
 	
 
