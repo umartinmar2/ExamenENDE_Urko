df5cb66da339af5735b10c7379c8b4ab4efb3a58
umartinmar2 <umartinmar2@educacion.navarra.es>
2025-12-19 08:59:13 +0100

    Constructor añadido

--- Examen_Urko/src/examen/Alquiler.java
+++ Examen_Urko/src/examen/Alquiler.java
@@ -3,7 +3,7 @@ package examen;
 public class Alquiler {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- Examen_Urko/src/examen/Vehiculo.java
+++ Examen_Urko/src/examen/Vehiculo.java
@@ -1,5 +1,21 @@
 package examen;
 
 public class Vehiculo {
+	String matricula;
+	String marca;
+	String modelo;
+	double precioHora;
+	
+	
+	public Vehiculo() {
+		super();
+		this.matricula = matricula;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.precioHora = precioHora;
+	}
+
+	
 
 }
+
